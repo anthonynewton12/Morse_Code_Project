4e9d826765d6ba5a77184c9c8621c97180ea806b
Anthony Newton <133197159+anthonynewton12@users.noreply.github.com>
2024-05-31 21:45:34 -0400

    Create ReceiveActivity.kt

---
+++ ReceiveActivity.kt
@@ -0,0 +1,16 @@
+package com.example.morsecodeproject
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class ReceiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receive)
+
+
+    }
+
+}
